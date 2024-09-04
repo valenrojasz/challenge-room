ff1febfcbb4f9d450b2cfb953167315fe60845ab
Valen Rojas <vlerojas193@gmail.com>
2024-09-04 02:50:10 -0500

    se crea la funcion contar_caracteres

---
+++ main.py
@@ -0,0 +1,25 @@
+def contar_fs(word):
+    count = 0
+    for i in range(len(word)):
+        if word[i] == ("F"):
+            count += 1
+            
+            return count
+        
+        def contar_fs(word):
+            return word.count("F")
+        
+        def contar_caracteres(word, letter):
+            count = 0
+            for i in range(len(word)):
+                if word[i] == letter:
+                    count += 1
+             return count
+        
+        print(contar_caracteres("FFffffFC", "f"))
+        print(contar_caracteres("FFffffFC", "C"))
+        print(contar_caracteres("FFffffFC", "F"))
+        
+        
+        
+        
